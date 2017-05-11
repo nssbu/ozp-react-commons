aba3afc2b959d99dc2ad19637d4ecaff77b6ea85
lsemesky <lsemesky@gmail.com>
2017-05-11 17:54:32 -0400

    updating see more style

--- app/js/components/notification/UserNotifications.jsx
+++ app/js/components/notification/UserNotifications.jsx
@@ -39,7 +39,7 @@ var UserNotifications = React.createClass({
                 <ul style={{'zIndex': '10000000'}} className="dropdown-menu UserNotifications">
                     { this._renderNotifications(this.props.func) }
                     <li>
-                      <button className="btn btn-primary btn-sm" onClick={() => this.props.moreNotifications()}>See more</button>
+                      <a href='#'><span style="width:100%; height: 100%;" onClick={() => this.props.moreNotifications()}>See more</span></a>
                     </li>
                 </ul>
             );
@@ -50,3 +50,4 @@ var UserNotifications = React.createClass({
 });
 
 module.exports = UserNotifications;
+//<button className="btn btn-primary btn-sm" onClick={() => this.props.moreNotifications()}>See more</button>
