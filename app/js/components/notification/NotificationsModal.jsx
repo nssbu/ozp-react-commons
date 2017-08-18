7529202c36c50a8e133193808ee664d10261e16f
Laura Semesky <lsemesky@gmail.com>
2017-08-18 18:45:19 -0400

    Refactored Notification modal into commons and separated into multiple react classes because yay, modularity!

---
+++ app/js/components/notification/NotificationsModal.jsx
@@ -0,0 +1,184 @@
+'use strict';
+
+var React = require('react');
+var Reflux = require('reflux');
+
+var Modal = require('../Modal.jsx');
+
+var { Navigation } = require('react-router');
+
+var SelfStore = require('../../stores/SelfStore');
+var SelfActions = require('../../actions/ProfileActions.js');
+var { API_URL } = require('../../OzoneConfig');
+
+ var marked = require('marked');
+ var renderer = new marked.Renderer();
+
+var NotificationsModal = React.createClass({
+    mixins: [Reflux.ListenerMixin],
+    getInitialState: function() {
+        return {
+            notificationList: []
+        };
+    },
+    componentWillMount: function() {
+        this.listenTo(SelfActions.fetchNotificationsCompleted, response => {
+            this.setState({
+                notificationList: response._response
+            });
+        });
+        this.listenTo(SelfActions.dismissNotificationCompleted, () => {
+            SelfActions.fetchNotifications();
+        })
+
+        SelfActions.fetchNotifications();
+    },
+    render: function() { 
+        console.log(this.state.notificationList)
+        return (
+            <Modal title="Notifications" ref="modal"
+            className="notification-window"
+            onCancel={this.close}>
+                <NotificationInfo notificationList={this.state.notificationList} />
+            </Modal>
+        );
+    },
+    close: function() {
+        var backRoute = this.props.backRoute;
+
+        if (typeof backRoute === 'function') {
+            backRoute();
+        }
+        else {
+            this.transitionTo(this.props.backRoute);
+        }
+    }
+   
+});
+
+var NotificationInfo = React.createClass({
+    propTypes: {
+        notificationList: React.PropTypes.array.isRequired,
+    },
+    getInitialState: function() {
+        return {
+            activeNotificationIndex: 0       
+        }
+    },
+    render: function() {
+        return (
+            <div className="row">
+            <div className="col-xs-4">
+            <ul className="nav nav-pills nav-inverse nav-stacked">
+                <NotificationSideBar activeNotificationIndex={this.state.activeNotificationIndex}
+                notificationList={this.props.notificationList} 
+                handleNotificationChange={this.handleNotificationChange}/>
+            </ul>
+            </div>
+            <div className="col-xs-8">
+            { this.props.notificationList.length > 0 &&
+                <Notification notification={this.props.notificationList[this.state.activeNotificationIndex]} />
+            }
+
+            { !this.props.notificationList.length &&
+                <span>No notifications</span>
+            }
+            </div>
+        </div>
+      );
+    },
+    handleNotificationChange: function(event, index) {
+        this.setState({activeNotificationIndex: index})
+    }
+});
+
+var Notification = React.createClass({
+    propTypes: {
+        notification: React.PropTypes.object
+    },
+    
+    render: function() { 
+        var notification = this.props.notification;
+        console.log(this.props.notification)
+        if(!this.props.notification)
+            return <div></div>
+        var createNotificationText = function() {
+            return {__html: marked(notification.message, { renderer: renderer })};
+          };
+          var date = new Date(notification.createdDate);
+          var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+          var formattedDate = months[date.getMonth()] + ' ' + date.getDate() + ', ' +  date.getFullYear();
+          return (
+            <div>
+              <div className="row" tabIndex={0}>
+                <h4>{(notification.listing) ? notification.listing.title : 'AppsMall'} <small>{formattedDate}</small></h4>
+                <div>
+    
+                  { notification.notificationType !== "peer_bookmark" &&
+                    <span className="message small" dangerouslySetInnerHTML={createNotificationText()}></span>
+                  }
+                  { notification.notificationType === "peer_bookmark" &&
+                    <div>
+                      <p className="message small">{notification.author.user.username} has shared a the folder <b>{notification.peer.folderName}</b> with you.</p>
+                      <p className="message small">{notification.message}</p>
+                      <div>
+                        <button className="btn btn-success btn-sm" onClick={() => {
+                            $.ajax({
+                                type: 'POST',
+                                dataType: 'json',
+                                contentType: 'application/json',
+                                url: API_URL + '/api/self/library/import_bookmarks/',
+                                data: JSON.stringify({
+                                  "bookmark_notification_id": notification.id
+                                })
+                            }).done(() => {
+                              this.onDismiss(notification);
+                            });
+                          }}>Add {notification.peer.folderName}</button>
+                      </div>
+                    </div>
+                  }
+                  <br /><br />
+                  <button className="btn btn-danger right" aria-label={`Remove notification from ${(notification.listing) ? notification.listing.title : 'AppsMall'}`} onClick={() => {
+                      this.onDismiss(notification);
+                    }}>
+                    Remove Notification
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+    },
+    onDismiss: function(){},
+});
+
+var NotificationSideBar = React.createClass({
+    propTypes: {
+        notificationList: React.PropTypes.array.isRequired,
+        activeNotificationIndex: React.PropTypes.number.isRequired,
+        handleNotificationChange: React.PropTypes.func.isRequired
+    },
+    render: function() {
+        var notis = this.props.notificationList.slice(0);
+        
+        return ( 
+            <div>
+            {notis.map((n, i) => {
+            var date = new Date(n.createdDate);
+            var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+            var formattedDate = months[date.getMonth()] + ' ' + date.getDate() + ', ' +  date.getFullYear();
+            return (
+                <li role="presentation" alt={`Notification ${i + 1} from ${(n.listing) ? n.listing.title : 'AppsMall'}`} tabIndex={i} 
+                onClick={(event) => {this.props.handleNotificationChange(event, i)}}>
+                <a href="#" onClick={(e) => {e.preventDefault()}}>
+                    {(n.listing) ? n.listing.title : 'AppsMall'} <small>{formattedDate}</small>
+                </a>
+                </li>
+            );
+            })}
+            </div>
+        );
+    }
+})
+
+module.exports = NotificationsModal;
